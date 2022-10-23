b48ac5881a2989f671c98dd1ed1adfb917bfbc28
JacquelineFM <jfloresm.dev@gmail.com>
2022-10-23 16:46:25 -0500

    feat: add basic app structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Container, Grid, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Container>
+      <header>
+        <Typography variant="h3" component="h1" align="center" marginY={5}>
+          Chews
+        </Typography>
+      </header>
+    </Container>
+  );
+};
+
+export default App;
